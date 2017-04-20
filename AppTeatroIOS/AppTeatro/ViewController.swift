3e53421c3d680d8da3014c016ca6f785c1ebf1bf
Lucas Farias <lucasfsiqueira282@gmail.com>
2017-04-20 17:09:11 -0300

    Criação de AppTeatro
    Criação do projeto AppTeatro.
    Inicio da modelagem de telas no storyBoard

---
+++ AppTeatroIOS/AppTeatro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppTeatro
+//
+//  Created by MyMac on 4/16/17.
+//  Copyright © 2017 MyMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
