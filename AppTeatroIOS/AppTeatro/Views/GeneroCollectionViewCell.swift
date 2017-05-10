52cf556a84574f4720c4e4ea53eb97d905cf2f14
Lucas Farias <lucasfsiqueira282@gmail.com>
2017-05-10 15:03:36 -0300

    PageView e telas 'Generos'
    Atualização do layout da pageView e criação da tela ‘Generos’

---
+++ AppTeatroIOS/AppTeatro/Views/GeneroCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  GeneroCollectionViewCell.swift
+//  AppTeatro
+//
+//  Created by MyMac on 5/10/17.
+//  Copyright © 2017 MyMac. All rights reserved.
+//
+
+import UIKit
+
+class GeneroCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgView_genero: UIImageView!
+    
+}
