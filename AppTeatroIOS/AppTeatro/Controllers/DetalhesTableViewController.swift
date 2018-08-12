4838cdbc686ffe255e2498a8a723b8df94c7bbec
lucasf282 <lucas.tico@hotmail.com>
2018-08-12 11:03:47 -0300

    Revert "Revert "Correção de erros, criação da tela 'meu perfil'""
    This reverts commit 01b9c606a09fe2ed55b12e08c2e518c166d907ac.

--- AppTeatroIOS/AppTeatro/Controllers/DetalhesTableViewController.swift
+++ AppTeatroIOS/AppTeatro/Controllers/DetalhesTableViewController.swift
@@ -9,16 +9,15 @@
 import UIKit
 
 class DetalhesTableViewController: UITableViewController {
+    let titulos = ["Sinopse",
+                   "elenco",
+                   "duração",
+                   "genero"]
+    
     let conteudo = ["Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.",
                     "Lucas Farias, Thiago Sousa e outros",
                     "2h e 34min",
                     "tragicomédia"]
-    
-    let titulos = ["Sinopse",
-                   "elenco",
-                   "duração",
-                   "genero"
-    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
